eef330c9d1ffe01425e0137a9186c966d157769e
Rain <ssiori@gmail.com>
2017-08-24 16:37:25 +0800

    up

--- app/src/main/java/io/github/yueeng/meituri/MainApplication.kt
+++ app/src/main/java/io/github/yueeng/meituri/MainApplication.kt
@@ -1,10 +1,6 @@
 package io.github.yueeng.meituri
 
 import android.app.Application
-import com.bumptech.glide.Glide
-import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
-import com.bumptech.glide.load.model.GlideUrl
-import java.io.InputStream
 import java.lang.ref.WeakReference
 
 
@@ -22,10 +18,5 @@ class MainApplication : Application() {
 
     init {
         app = WeakReference(this)
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        Glide.get(this).registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(okhttp))
     }
 }
